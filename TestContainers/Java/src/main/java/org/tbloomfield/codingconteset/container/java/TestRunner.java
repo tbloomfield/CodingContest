780b6a759bc4c44820591e15ea3525ff59fe455f
tbloomfield <tom.bloomfield@gmail.com>
2024-04-22 15:44:30 -0500

    Swagger and Docker support

--- TestContainers/Java/src/main/java/org/tbloomfield/codingconteset/container/java/TestRunner.java
+++ TestContainers/Java/src/main/java/org/tbloomfield/codingconteset/container/java/TestRunner.java
@@ -1,7 +1,9 @@
 package org.tbloomfield.codingconteset.container.java;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class TestRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(TestRunner.class, args);
--- TestContainers/Java/src/main/java/org/tbloomfield/codingconteset/container/java/server/TestRunnerController.java
+++ TestContainers/Java/src/main/java/org/tbloomfield/codingconteset/container/java/server/TestRunnerController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.tbloomfield.codingconteset.container.java.executor.TestCase;
 
 @RestController
+@RequestMapping("/codeRunner")
 public class TestRunnerController {
 	
 	@PostMapping("/execute")
